e81331def178b960ed7ab600869af6a92404b2d0
PavelDrg <pavel.dragan2@gmail.com>
2024-03-15 16:00:20 +0200

    added color detection functionality

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import cv2
+from PIL import Image
+
+from util import get_limits
 
 
+color = [0, 255, 0]  # color in BGR colorspace
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    print(bbox)
 
     cv2.imshow('Conductor Cam', frame)
 
